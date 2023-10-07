f089670b55f85d1b2ca3f5a0846538d42bb08daf
madmax7896 <madmax78968@gmail.com>
2023-10-07 23:24:26 +0530

    Apollo: Import Dolby audio blobs

--- device.mk
+++ device.mk
@@ -276,6 +276,9 @@ PRODUCT_PACKAGES += \
     media_codecs_performance_kona.xml \
     video_system_specs.json
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml
+    
 $(call soong_config_set,stagefright,target_disable_thumbnail_block_model,true)
 
 # NFC
