175ed8a986437e0cd52eb943891c1cd2327d5015
Quallenauge <Hamsi2k@freenet.de>
2024-04-18 08:24:17 +0300

    apollo: libc: Force build scudo free 32 bit variant of libc.
    We have issues with camera blobs which doesn't play
    well with scudo allocation/deallocation tracking.
    Scudo reports corrupted header when stopping a
    video recording.
    
    Because we aren't alone with this (also upstream seems
    to have trouble with camera modules) use the 32 libc
    which is used only by a few components, like camera
    and audio.
    
    Because docs states, that libc with the svelte take less RSS,
    but is a little bit slower.
    https://android.googlesource.com/platform/bionic/+/master/docs/native_allocator.md
    This is the reason to not enable this target wide.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -48,6 +48,9 @@ TARGET_NO_BOOTLOADER := true
 # Camera - HIDL overrideFormat
 TARGET_CAMERA_USES_NEWER_HIDL_OVERRIDE_FORMAT = true
 
+# Camera Malloc
+MALLOC_SVELTE_FOR_LIBC32 := true
+
 # Camera - Miui
 TARGET_CAMERA_PACKAGE_NAME := com.android.camera
 
