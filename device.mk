8114d4eff4075002bdf5f3abb054ee4499a0f70d
Flicker372 <flicker372@outlook.com>
2024-12-02 19:57:11 +0800

    Revert "sm8250-common: remove OMX"
    This reverts commit 931a013de75d9aea4e82e85de01a7920c9810caf.

--- device.mk
+++ device.mk
@@ -272,6 +272,8 @@ endif
 PRODUCT_PACKAGES += \
     media_codecs_c2.xml \
     media_codecs_performance_c2.xml \
+    media_codecs_kona.xml \
+    media_codecs_performance_kona.xml \
     video_system_specs.json
 
 $(call soong_config_set,stagefright,target_disable_thumbnail_block_model,true)
@@ -293,7 +295,16 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_nfc/com.nxp.mifare.xml
 
 # OMX
-TARGET_SUPPORTS_OMX_SERVICE := false
+PRODUCT_PACKAGES += \
+    libOmxAacEnc \
+    libOmxAmrEnc \
+    libOmxCore \
+    libOmxEvrcEnc \
+    libOmxG711Enc \
+    libOmxQcelp13Enc \
+    libOmxVdec \
+    libOmxVenc \
+    libstagefrighthw
 
 # Miui Camera
 include device/xiaomi/camera/miuicamera.mk
