fcbff88ae3d36248596cfb4bd36dacbb67a33a36
MurtazaKolachi <murtazakolachi7@gmail.com>
2025-04-08 01:49:05 +0500

    apollo: Evolution X bringup

--- lineage_apollo.mk
+++ lineage_apollo.mk
@@ -12,6 +12,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 TARGET_BOOT_ANIMATION_RES := 1080
+WITH_GMS := true
+TARGET_USES_MINI_GAPPS := true
+BUILD_BCR := true
+
+ifeq ($(TARGET_USES_MINI_GAPPS),true)
+PRODUCT_PACKAGES += \
+    TurboAdapter
+endif
+TARGET_DISABLE_EPPE := true
 
 # Inherit from apollo device
 $(call inherit-product, device/xiaomi/apollo/device.mk)
