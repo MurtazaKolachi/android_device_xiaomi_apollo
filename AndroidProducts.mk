654f6d717440a6236814b3b5474bef121ce9e900
Murtaza Hussain <160129300+MurtazaKolachi@users.noreply.github.com>
2025-11-22 00:10:15 +0500

    Apollo: Welcome @Project-Mica

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_apollo.mk
+    $(LOCAL_DIR)/mica_apollo.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_apollo-user \
-    lineage_apollo-userdebug \
-    lineage_apollo-eng
+    mica_apollo-user \
+    mica_apollo-userdebug \
+    mica_apollo-eng
