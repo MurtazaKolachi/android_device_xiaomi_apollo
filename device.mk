5c158bd4382150b16ff0f0ae44082f358ec4f008
D-ArtGit <dartx79@gmail.com>
2025-12-02 22:27:56 +0300

    Fix incoming call ringtone on Bluetooth Orig: https://review.lineageos.org/q/topic:%22skip_speaker_layout_channel_mask_field%22
    (cherry picked from commit 3ff424a44c8c466591d8f6c14f5c4610e4ed433d)

--- device.mk
+++ device.mk
@@ -119,6 +119,8 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/hearing_aid_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/hearing_aid_audio_policy_configuration.xml
 
+$(call soong_config_set,android_hardware_audio,skip_speaker_layout_channel_mask_field,true)
+
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
